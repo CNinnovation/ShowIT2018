4def4eeb9dd722c091b05d907c84f049410aed98
Christian Nagel <christian@christiannagel.com>
2018-01-29 21:38:54 +0100

    add launching uwp from wpf

--- 02_DesktopBridge/WPFDISample/MainWindow.xaml.cs
+++ 02_DesktopBridge/WPFDISample/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using DISampleViewModels.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows;
+using Windows.System;
 
 namespace WPFDISample
 {
@@ -13,5 +15,12 @@ namespace WPFDISample
         }
 
         public MainViewModel ViewModel => ApplicationServices.Instance.ServiceProvider.GetService<MainViewModel>();
+
+        private async void OnLaunchUWP(object sender, RoutedEventArgs e)
+        { 
+            string protocol = "showit2018";
+            var uri = new Uri($"{protocol}://");
+            var success = await Launcher.LaunchUriAsync(uri);
+        }
     }
 }
