7f51d970bce52ec45b3825714a9802598be15c57
Christian Nagel <christian@christiannagel.com>
2018-01-29 17:40:27 +0100

    shared view-models

---
+++ 03_SharedViewModels/WPFDISample/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DISampleViewModels.ViewModels;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Windows.ApplicationModel.DataTransfer;
+using System;
+using Windows.System;
+
+namespace WPFDISample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public MainViewModel ViewModel => ApplicationServices.Instance.ServiceProvider.GetService<MainViewModel>();
+
+    }
+}
