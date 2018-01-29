ee6eb2d8992668d33a1822f51c7b74375383cafb
Christian Nagel <christian@christiannagel.com>
2018-01-29 17:26:34 +0100

    desktop bridge

---
+++ 02_DesktopBridge/WPFDISample/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DISampleViewModels.ViewModels;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Windows.ApplicationModel.DataTransfer;
+using System;
+using Windows.System;
+
+namespace WPFDISample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public MainViewModel ViewModel => ApplicationServices.Instance.ServiceProvider.GetService<MainViewModel>();
+
+        private async void OnShare(object sender, RoutedEventArgs e)
+        {
+            string protocol = "sampleprotocol";
+            Uri uri = new Uri($"{protocol}://");
+            var success = await Launcher.LaunchUriAsync(uri);
+
+
+        }
+
+        private void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            args.Request.Data.SetText("Hello from WPF");
+        }
+    }
+}
